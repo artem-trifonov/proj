6a7d9a9ff3b886c4201d2a91c6b0b3c51202a1b2
Artem Trifonov <trifonov.artem.nn@gmail.com>
2025-10-16 09:25:18 +0300

    Добавлен класс Изменена логика

--- src/main/java/com/stepup/proj/MainAppl.java
+++ src/main/java/com/stepup/proj/MainAppl.java
@@ -3,6 +3,6 @@ package com.stepup.proj;
 public class MainAppl {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("11111");
+        System.out.println("1");
     }
 }
