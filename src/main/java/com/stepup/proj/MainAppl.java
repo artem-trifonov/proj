82d4470b00d652cc5f4cc700846e37b3ea2be284
Artem Trifonov <trifonov.artem.nn@gmail.com>
2025-10-16 09:42:59 +0300

    Добавлен вызов метода

--- src/main/java/com/stepup/proj/MainAppl.java
+++ src/main/java/com/stepup/proj/MainAppl.java
@@ -5,6 +5,7 @@ public class MainAppl {
         System.out.println("----");
         System.out.println("Hello world");
         System.out.println("5");
+        doSomething();
         System.out.println("121");
     }
 
