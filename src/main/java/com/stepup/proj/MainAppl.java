f16a56b0fbd4b4380b68aeddfffba1039a708109
Artem Trifonov <trifonov.artem.nn@gmail.com>
2025-10-16 09:31:06 +0300

    Новая логика

--- src/main/java/com/stepup/proj/MainAppl.java
+++ src/main/java/com/stepup/proj/MainAppl.java
@@ -2,7 +2,8 @@ package com.stepup.proj;
 
 public class MainAppl {
     public static void main(String[] args) {
+        System.out.println("----");
         System.out.println("Hello world");
-        System.out.println("1");
+        System.out.println("121");
     }
 }
