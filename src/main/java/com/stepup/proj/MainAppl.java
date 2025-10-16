51f1a0655fb700f74ac15c1f3256dad7e0edf1d7
Artem Trifonov <trifonov.artem.nn@gmail.com>
2025-10-16 09:16:27 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainAppl.java
+++ src/main/java/com/stepup/proj/MainAppl.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainAppl {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("11111");
     }
 }
